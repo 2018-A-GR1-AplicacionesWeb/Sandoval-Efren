2ac868cd225c881688acf7c1a07ad882fe5102af
efren sandoval <efren.sandoval@epn.edu.ec>
2018-05-21 10:12:37 -0500

    CONECTANDO A LA BD
    Estableciendo la conexion a la base del profe y creando entidades

--- 03NestJS/src/app.module.ts
+++ 03NestJS/src/app.module.ts
@@ -1,10 +1,55 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module, NestModule, MiddlewareConsumer} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UsuarioController} from "./usuario.controller";
+import {UsuarioService} from "./usuario.service";
+import {ParametrosController} from "./parametros.controller";
+import {LogMiddleware} from './log.middleware';
+
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {UsuarioEntity} from "./usuario/usuario.entity";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'web2018agr2.mysql.database.azure.com',
+            port: 3306,
+            username: 'profesor@web2018agr2',
+            password: 'Javascript1',
+            database: 'web',
+            entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+            synchronize: true,
+            ssl: true
+        }),
+        TypeOrmModule.forFeature([
+            UsuarioEntity
+        ])
+    ],
+    controllers: [
+        AppController,
+        UsuarioController,
+        ParametrosController],
+    providers: [
+        AppService,
+        UsuarioService
+    ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    nombreAplicacion = 'EPN';
+
+    configure(consumer: MiddlewareConsumer)
+        : void {
+        consumer
+            .apply(LogMiddleware)
+            .with(this.nombreAplicacion, 1989)
+            .forRoutes(
+                UsuarioController,
+                AppController,
+                ParametrosController
+            )
+        //.apply(OtroMiddleware)
+        //.forRoutes(Otras rutas);
+    }
+
+}
---
+++ 03NestJS/src/usuario/usuario.entity.ts
@@ -0,0 +1,13 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity('web_eguezv_usuario')
+export class UsuarioEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({length: 50})
+    nombre: string;
+
+
+}
